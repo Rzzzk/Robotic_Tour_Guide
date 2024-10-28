8ded62952a78cba14a80a95144b37ed83de69e46
Rezk Ahmed <rezksh55112@gmail.com>
2024-10-28 14:31:12 +0300

    ws setup

---
+++ App/main.c
@@ -0,0 +1,17 @@
+
+
+#include "main.h"
+
+int main(void)
+{
+	SystemClock_Config();
+
+
+	while (1)
+	{
+
+	}
+
+}
+
+
---
+++ App/main.h
@@ -0,0 +1,9 @@
+
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "hal.h"
+
+
+
+#endif /* __MAIN_H */
